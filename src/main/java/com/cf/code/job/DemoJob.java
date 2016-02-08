e23aa1c5ffd014cc197e0d3d12870d899c38fa30
congfeng <congfeng@91nongye.com>
2016-02-08 12:37:37 +0800

    init

--- src/main/java/com/cf/code/job/DemoJob.java
+++ src/main/java/com/cf/code/job/DemoJob.java
@@ -3,14 +3,6 @@
  */
 package com.cf.code.job;
 
-import javax.annotation.Resource;
-
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
-import com.jisheng.peisong.exception.BusinessException;
-import com.jisheng.peisong.service.DemoService;
-
 /**
  *                       
  * @Filename: DemoJob.java
@@ -20,41 +12,5 @@ import com.jisheng.peisong.service.DemoService;
  *
  */
 public class DemoJob {
-	
-	private static final Logger log = LogManager.getLogger(DemoJob.class);
-
-	@Resource(name = "demoService")
-	DemoService demoService;
-	
-	@Resource(name = "demoService_dubbo")
-	DemoService demoService_dubbo;
-	
-	private String sign;
-	
-	public DemoJob(){
-		
-	}
-	
-	public DemoJob(String sign){
-		this.sign = sign;
-	}
-	
-	public String doitA(){
-		this.demoService.insert("doitA"+"-----"+this+"--------"+this.sign);
-		log.info("doitA"+"-----"+this.sign);
-		return "doitA"+"-----"+this.sign;
-	}
-	
-	public String doitB(){
-		try {
-			this.demoService.update(6,"doitB"+"-----"+this+"--------"+this.sign);
-		} catch (BusinessException e) {
-			this.demoService.insert("doitB-update-error"+"-----"+this+"--------"+this.sign);
-		}
-		log.info("doitB"+"-----"+this.sign);
-		return "doitB"+"-----"+this.sign;
-	}
-	
-	
 	
 }
