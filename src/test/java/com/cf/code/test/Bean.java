633e5923542745e7e0928aac887262739303578f
congfeng <3024992@qq.com>
2016-03-31 23:45:43 +0800

    socketio

---
+++ src/test/java/com/cf/code/test/Bean.java
@@ -0,0 +1,16 @@
+package com.cf.code.test;
+
+public class Bean {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
--- src/test/java/com/cf/code/test/TestMain.java
+++ src/test/java/com/cf/code/test/TestMain.java
@@ -3,12 +3,12 @@
  */
 package com.cf.code.test;
 
-import java.net.URLDecoder;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
+import com.corundumstudio.socketio.AckRequest;
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.DataListener;
 
-import com.cf.code.common.DateUtil;
 
 /**
  * @Version: 1.0
@@ -20,28 +20,37 @@ public class TestMain {
 	static int i = 100;
 	
 	public static void main(String[] args) {
-//		Predicate<String> xx = (d) -> d.length() > 5;
-//		Function<String,String> d1 = (p) -> p+"-1";
-//		Function<String,String> d2 = (p) -> p+"-2";
-//		Function<String,String> dx = d1.compose(d2);
-//		System.out.println(dx.apply("congfeng"));
-//		Supplier<Integer> s = Test::testd;
-//		System.out.println(s.get());
-//		Consumer<String> c = (p) -> System.out.println(p+"x");
-//		c.accept("y");
-//		Optional<String> op = Optional.of("abcd");
-//		op.isPresent();
-//		op.get();
-//		op.orElse("other");
-//		op.ifPresent(System.out::print);
-//		Map<Integer, String> map = new HashMap<>();
-//		map.putIfAbsent(5, "b");
-//		map.putIfAbsent(5,(x,y)->x+","+y);
-//		map.computeIfPresent(5,(x,y)->null);
-//		System.out.println(map.get(5));
-//		System.out.println(DateUtil.toParse("2016-03-22 10:57:00").getTime()/1000);
-		String d = "%e6%9c%aa%e5%88%86%e9%a1%9e";
-		System.out.println(URLDecoder.decode(d));
+		Configuration config = new Configuration();
+        config.setHostname("localhost");
+        config.setPort(9093);
+        final SocketIOServer server = new SocketIOServer(config);
+//        server.addEventListener("chatevent", ChatObject.class, new DataListener<ChatObject>(){
+//        	public void onData(SocketIOClient client, ChatObject data, ackSender arg2) throws Exception {
+//        		
+//        	};
+//        });
+        server.addEventListener("mychatevent", Bean.class, new DataListener<Bean>(){
+			@Override
+			public void onData(SocketIOClient client, Bean data, AckRequest ackSender)throws Exception {
+				System.out.println("mychatevent-------------"+data.getName());
+				//server.getBroadcastOperations().sendEvent("mychatevent", data);
+			}
+        });
+        server.start();
+        
+        int i = 0;
+        while(true){
+        	try {
+    			Thread.sleep(5000) ;
+    		} catch (InterruptedException e) {
+    			e.printStackTrace();
+    		}
+        	Bean bean = new Bean();
+            bean.setName("ddddd"+i++);
+            server.getBroadcastOperations().sendEvent("mychatevent", bean);
+        }
+        
+       // server.stop();
 	}
 	
 	
