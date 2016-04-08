aab8443e6ad3b176569a13678b5bd07407203218
congfeng <congfeng@91nongye.com>
2016-04-08 18:05:30 +0800

    init

---
+++ src/main/webapp/js/preview.js
@@ -0,0 +1,5 @@
+$(function(){
+    uParse('#richtext',{
+        rootPath : '/resources/ueditor1_4_3_2/'
+    })
+})
--- src/main/webapp/js/productadd.js
+++ src/main/webapp/js/productadd.js
@@ -66,7 +66,22 @@ nsApp.controller('ProductAddController',function($scope,$routeParams) {
 	});
 	
 	$(".p-preview-btn").click(function(){
-		showAlert('功能建设中....');
+		//showAlert('功能建设中....');
+		layer.open({
+			type: 2,
+			shadeClose: true,
+			title: ['商品详情预览', 'font-size:18px;color:green;'],
+			offset:'10px',
+			area: '716px',
+			content:['/pages/preview.html','no'],
+			maxHeight:'500px',
+			success:function(l,i){
+				var previewJQdom = $($($(l[0]).find('iframe')[0]).contents().get(0));
+				$(previewJQdom.find('#productName')[0]).text($('#name').val());
+				$(previewJQdom.find('#richtext')[0]).html(ue.getContent());
+				layer.iframeAuto(i);
+			}
+		});
 	});
 	
 });
